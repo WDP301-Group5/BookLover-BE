48603cfebc9dbf82a033dc6a6707656289839994
Bernie Truong <tqtolympia@gmail.com>
2026-02-02 11:24:29 +0700

    feat: add authentication module with Google OAuth support
    Introduced authentication controller, service, and routes to handle user authentication,
    including Google OAuth integration.
    Added input validation utilities and updated hashPassword utility.
    Updated .gitignore and added new dependencies: google-auth-library and zod.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import authRouter from "./authRoutes.js";
 import userRouter from "./userRoutes.js";
 
 const routes = express.Router();
 
+routes.use("/auth", authRouter);
 routes.use("/users", userRouter);
 
 export default routes;
