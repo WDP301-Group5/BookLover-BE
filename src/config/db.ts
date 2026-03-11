aef3c74bdd30b9317f4ff9113335d57ba49a7dd0
vietnt1904 <thanhviet01092004@gmail.com>
2026-03-11 19:20:28 +0700

    update: history table

--- src/config/db.ts
+++ src/config/db.ts
@@ -36,7 +36,7 @@ const connectDB = async (): Promise<void> => {
 		}
 	} catch (err) {
 		console.error("❌ MongoDB connection failed:", err);
-		process.exit(1);
+		// process.exit(1);
 	} finally {
 		if (timeoutHandle) {
 			clearTimeout(timeoutHandle);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,18 +1,19 @@
 import express from "express";
-import { addNewReadingHistory } from "../controllers/readingHistoryController";
+import { addNewReadingHistory, deleteHistory } from "../controllers/readingHistoryController";
 import {
 	getLast3History,
 	getProfile,
 	updateProfile,
 } from "../controllers/userController";
-import { verifyToken } from "../middleware/auth";
+import { checkToken, verifyToken } from "../middleware/auth";
 import { uploadAvatarAndBackground } from "../middleware/upload";
 
 const userRouter = express.Router();
 
 // TEST endpoint removed - use /admin/users instead
-userRouter.get("/history/last3", getLast3History);
+userRouter.get("/history/last3", checkToken, getLast3History);
 userRouter.put("/history/reading", addNewReadingHistory);
+userRouter.delete("/history/:historyId", checkToken, deleteHistory);
 
 userRouter.get("/profile", verifyToken, getProfile);
 userRouter.put(
